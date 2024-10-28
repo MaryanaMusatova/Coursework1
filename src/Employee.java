b2094244a82b1d9e175d9c00e0fcc95405c3d83c
https://github.com/MaryanaMusatova/Homework1 <musatovam27@gmail.com>
2024-10-28 13:21:04 +0300

    Курсовая работа №1

--- src/Employee.java
+++ src/Employee.java
@@ -15,20 +15,68 @@ public class Employee {
     }
 
     public String getFullName() {
-        return this.fullName;
+        return fullName;
     }
+
     public int getDepartment() {
         return department;
     }
+
     public int getSalary() {
         return salary;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getIdCount() {
+        return idCount;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
     public void setDepartment(int department) {
         this.department = department;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void setIdCount(int idCount) {
+        Employee.idCount = idCount;
+    }
+
+    public static Employee createEmployee(String fullName, int department, int salary) {
+        Employee employee = new Employee(fullName, department, salary);
+        return employee;
+    }
+
+    public static void namedEmployee() {
+        Employee[] employees = new Employee[10];
+        employees[0] = createEmployee("Абашин Иван Сергеевич", 1, 147500);
+        employees[1] = createEmployee("Бочарников Виталий Андреевич", 3, 99560);
+        employees[2] = createEmployee("Гоппе Дмитрий Валентинович", 5, 163000);
+        employees[3] = createEmployee("Ефимов Николай Александрович", 2, 89400);
+        employees[4] = createEmployee("Костромина Анна Анатольевна", 1, 125000);
+        employees[5] = createEmployee("Лукьянов Юрий Викторович", 4, 110700);
+        employees[6] = createEmployee("Морозова Валерия Юрьевна", 3, 107000);
+        employees[7] = createEmployee("Никифоров Андрей Андреевич", 4, 170400);
+        employees[8] = createEmployee("Петренко Оксана Георгиевна", 2, 100800);
+        employees[9] = createEmployee("Самохвалов Александр Петрович", 5, 157000);
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -45,6 +93,7 @@ public class Employee {
 
     @Override
     public String toString() {
-        return id +" "+ fullName+ " "+department+" "+salary;
+        return id + " " + fullName + " " + department + " " + salary;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
 public class Main {
     private static Employee[] employees = new Employee[10];
 
-    public static Employee createEmployee(String fullName, int department, int salary) {
-        Employee employee = new Employee(fullName, department, salary);
-        return employee;
+    public static void main(String[] args) {
+        System.out.println("Курсовое задание. Книга сотрудников. Базовая сложность");
+        Employee.namedEmployee();
+        int result = calculateSalary();
+        System.out.println("Общая сумма затрат на зарплату составляет " + result + " руб");
+        System.out.println("Сотрудник с минимальной зарплатой: " + getEmployeeMinSalary());
+        System.out.println("Сотрудник с максимальной зарплатой: " + getEmployeeMaxSalary());
+        System.out.println("Средняя зарплата: " + getMiddleSalary());
+        printFullNames();
     }
 
-    private static int calculateSalary(Employee[] employees) {
-        int result = 0;
+    private static int calculateSalary() {
+        int result = employees[0].getSalary();
         for (int i = 0; i < employees.length; i++) {
             Employee employee = employees[i];
-            if (employee != null) {
-                result = result + employee.getSalary();
-            }
+            result = result + employee.getSalary();
         }
         return result;
     }
 
-    public static Employee getEmployeeMinSalary(Employee[] employees) {
-        Employee employeeMinSalary = null;
+    public static Employee getEmployeeMinSalary() {
+        Employee employeeMinSalary = employees[0];
         for (Employee employee : employees) {
-            if (employeeMinSalary == null || employee.getSalary() < employeeMinSalary.getSalary()) {
+            if (employee.getSalary() < employeeMinSalary.getSalary()) {
                 employeeMinSalary = employee;
             }
         }
@@ -28,53 +32,24 @@ public class Main {
 
     }
 
-    public static Employee getEmployeeMaxSalary(Employee[] employees) {
-        Employee employeeMaxSalary = null;
+    public static Employee getEmployeeMaxSalary() {
+        Employee employeeMaxSalary = employees[0];
         for (Employee employee : employees) {
-            if (employeeMaxSalary == null || employee.getSalary() > employeeMaxSalary.getSalary()) {
+            if (employee.getSalary() > employeeMaxSalary.getSalary()) {
                 employeeMaxSalary = employee;
             }
         }
         return employeeMaxSalary;
-
     }
 
-    public static double getMiddleSalary(Employee[] employees) {
-        return (double) calculateSalary(employees) / employees.length;
+    public static double getMiddleSalary() {
+        return (double) calculateSalary() / employees.length;
     }
 
-    public static void printFullNames(Employee[] employees) {
+    public static void printFullNames() {
         for (Employee employee : employees) {
             System.out.println(employee.getFullName() + " ");
-
         }
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("Курсовое задание. Книга сотрудников. Базовая сложность");
-        Employee[] employees = new Employee[10];
-        employees[0] = createEmployee("Абашин Иван Сергеевич", 1, 147500);
-        employees[1] = createEmployee("Бочарников Виталий Андреевич", 3, 99560);
-        employees[2] = createEmployee("Гоппе Дмитрий Валентинович", 5, 163000);
-        employees[3] = createEmployee("Ефимов Николай Александрович", 2, 89400);
-        employees[4] = createEmployee("Костромина Анна Анатольевна", 1, 125000);
-        employees[5] = createEmployee("Лукьянов Юрий Викторович", 4, 110700);
-        employees[6] = createEmployee("Морозова Валерия Юрьевна", 3, 107000);
-        employees[7] = createEmployee("Никифоров Андрей Андреевич", 4, 170400);
-        employees[8] = createEmployee("Петренко Оксана Георгиевна", 2, 100800);
-        employees[9] = createEmployee("Самохвалов Александр Петрович", 5, 157000);
-
-        for (int i = 0; i < employees.length; i++) {
-            System.out.println(employees[i]);
-        }
-
-        int result = calculateSalary(employees);
-        System.out.println("Общая сумма затрат на зарплату составляет " + result + " руб");
-        System.out.println("Сотрудник с минимальной зарплатой: " + getEmployeeMinSalary(employees));
-        System.out.println("Сотрудник с максимальной зарплатой: " + getEmployeeMaxSalary(employees));
-        System.out.println("Средняя зарплата: " + getMiddleSalary(employees));
-        printFullNames(employees);
-
-    }
 }
